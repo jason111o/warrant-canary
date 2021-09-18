5f89b1a354762981f29d4820308708cf3c2d3d8f
Moe Tox <moetox@localhost.localdomain>
2021-09-18 06:59:58 -0500

    Now has cli output as well as notify-send

--- installer.sh
+++ installer.sh
@@ -1,47 +1,91 @@
 #!/usr/bin/env bash
 
 
+##### Add a little style
+RED='\033[01;31m'
+YELLOW='\033[01;33m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+NOCOLOR='\033[0m'
+
+
 #### Check for super powers
 if [ $EUID != 0 ]; then
-  echo -e "\nInstaller must be run with Super Powers ;)\n"
+  echo -e "\n${RED}Installer must be run with Super Powers ;)\n${NOCOLOR}"
+  exit 1
+fi
+#### Ensure that we are in the warrant-canary directory
+if [[ $(echo $PWD | awk -F '/' '{print $NF}') != "warrant-canary" ]]; then
+  echo -e "\n${RED}Installer must be run from the warrant-canary directory\n${NOCOLOR}"
   exit 1
 fi
 
 
-#### Check that dependencies are satisfied
-which fping
-if [ $? == 1 ]; then
-  echo -e "\nThis script requires fping to be installed"
-  echo "Would you like to install it now? y/n"
-  read ans
-  if [[ $ans == 'y' ]]; then
-    apt install -f -y fping
-  else
-    echo -e "\nExiting without installation"
-    exit 1
-  fi
-fi
-which curl
-if [ $? == 1 ]; then
-  echo -e "\nThis script also requires curl to be installed"
-  echo "Would you like to install it now? y/n"
-  read ans
-  if [[ $ans == 'y' ]]; then
-    apt install -f -y curl
-  else
-    echo -e "\nRemoving fping..."
-    sleep 2
-    apt remove -f -y fping
-    echo -e "\nExiting without installation"
-    exit 1
-  fi
-fi
+#### Check for dependencies (some are moot, purely for cli asthetics)
+check_for_dependencies() {
+  depends=("curl" "fping" "echo" "apt" "printf")
+  depends_not_installed=()
+  printf "${WHITE}Checking dependencies"
+  for x in "${depends[@]}"; do
+    printf "."
+    xpath=$(command -v "$x")
+    if [[ "$xpath" ]]; then
+      sleep .5
+    elif [[ ! "$xpath" ]]; then
+      sleep 0.25
+      depends_not_installed+=("$x")
+    fi
+  done
+  echo ""
+  sleep 1
+}
+#### If dependencies are needed then prompt the user to install them
+dependencies_needed() {
+  len=${#depends_not_installed}
+  while [ "$len" -gt 0 ]; do
+    echo ""
+    echo -e "${WHITE}Install missing dependencies? Y/n/e/v${NOCOLOR}"
+    read -r ans
+    if [[ "$ans" == "v" ]]; then
+      print_version
+    elif [[ "$ans" == "n" ]]; then
+      echo -e "${CYAN}This script needs..."
+      for x in "${depends_not_installed[@]}"; do
+        echo -e "${WHITE}----> ${RED}$x"
+        sleep 0.5
+      done
+      echo -e "${CYAN}in order to work.${NOCOLOR}\n"
+    elif [[ $ans == "e" ]]; then
+      echo -e "${WHITE}Exiting...${NOCOLOR}\n"
+      sleep 1
+      exit 1
+    elif [[ "$len" == 0 ]]; then
+      break
+    else
+      for x in "${depends_not_installed[@]}"; do
+        apt install -f -y "$x"
+        if [[ $? != 0 ]]; then
+          echo -e "${YELLOW}\"$x\" was not found in your distribution package sources."
+          echo -e "Check your distros documentation.${NOCOLOR}\n"
+        fi
+        check_for_dependencies
+        len=${#depends_not_installed}
+        if [[ $len == 0 ]]; then
+          echo -e "${WHITE}Dependencies have been satisfied. Press enter to continue...${NC}"
+        fi
+      done
+    fi
+  done
+}
+#### Run previously declared funtions
+check_for_dependencies
+dependencies_needed
 
 
 #### Check if warrant_canary is already installed
 if [ -x /usr/local/bin/warrant_canary ]; then
-  echo "warrant_canary is already installed"
-  echo "Press ENTER to reinstall or CTRL+C to exit"
+  echo -e "\n${YELLOW}Warrant Canary is already installed."
+  printf "${CYAN}Press ENTER to reinstall or CTRL+C to exit"
   read
 fi
 
@@ -53,13 +97,15 @@ fi
 
 
 #### Install everything to /usr/local/bin and set permissions for all users
+echo -e "\n${WHITE}Installing Warrant Canary...${NOCOLOR}"
+sleep 3
 cp source/script/warrant_canary.sh /usr/local/bin/warrant_canary
 chmod +x /usr/local/bin/warrant_canary
 cp -r source/warrant_canary_icons /usr/local/bin/
 
 
 #### Everything is done
-echo -e "\nwarrant_canary has been installed and can be run from the command line as 'warrant_canary'\n"
+echo -e "\n${CYAN}Warrant Canary has been installed and can be run from the command line as 'warrant_canary'\n${NCOCOLOR}"
 
 
 exit 0
--- source/script/warrant_canary.sh
+++ source/script/warrant_canary.sh
@@ -3,46 +3,60 @@
 ####MOETOX####
 
 #### Set some variables
-version="Warrant Canary v1.0"
+version="Warrant Canary v1.1"
 icons="/usr/local/bin/warrant_canary_icons"
 phrase="Signed Warrant"
 website="https://parrotsec.org/docs/warrant-canary.html"
 
 #### Display version info
 notify-send -t 3000 -i $icons/still_safe.png "$version"
+echo "$version"
 sleep 3
 
 #### Check for internet connection a time or two if no connection is made
+connected="true"
 tries=3
-while true; do
+while (expr $tries > 0); do
     notify-send -t 3000 -i $icons/check_connection.png "Checking internet connection"
+    echo "Checking internet connection..."
+    fping -4 parrotsec.org &
     sleep 3
-  connected=$(fping -4 google.com 2>/dev/null | grep -o alive)
+  connected=$(fping -4 parrotsec.org 2>/dev/null | grep -o alive)
   if [[ $connected == "alive" ]]; then
     notify-send -t 3000 -i $icons/connected.png "Connection established!"
+    echo "Connection established!"
     sleep 3
     notify-send -t 3000 -i $icons/check_canary.png "Checking for Canary Warrant"
+    echo "Checking for Canary Warrant"
     sleep 3
     break
   else
     notify-send -t 3000 -i $icons/not_connected.png "You are not online!"
+    echo "You are not online!"
     sleep 3
     notify-send -t 3000 -i $icons/check_connection.png "Trying again ($tries)..."
-    sleep 3
-  fi
-  if [[ $tries == 0 ]]; then
-    notify-send -t 3000 -i $icons/not_connected.png "Connection cannot be established! Exiting..."
-    exit 99
+    echo "Trying again ($tries)"
   fi
   tries=$(expr $tries - 1)
+  if [[ $tries == 0 ]]; then
+    connected="false"
+  fi
 done
+if [[ "$connected" == "false" ]]; then
+    notify-send -t 3000 -i $icons/not_connected.png "Connection cannot be established! Exiting..."
+    echo "Connection cannot be established! Exiting..."
+    sleep 1
+  exit 99
+fi
 
 #### Now actually check for the ParrotSEC warrant canary
 #### Well... at least a particular phrase
 keywords=$(curl -L -s $website | grep "$phrase")
 if [[ $? != 0 ]]; then
     notify-send -t 0 -i $icons/has_warrants.png "PARROTSEC MAY HAVE BEEN SERVED!!!"
+    echo "PARROTSEC MAY HAVE BEEN SERVED!!!"
   else
     notify-send -t 10000 -i $icons/still_safe.png "$keywords"
+    echo "$keywords"
 fi
 exit 0
